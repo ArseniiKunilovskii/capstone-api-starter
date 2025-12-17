933afee6eb99c5dc6bb3e8a652179aa0378bf3d6
arsenii <arseniy.kunilovskiy@gmail.com>
2025-12-17 13:18:40 -0500

    DELETE cart request is added

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCardDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCardDao.java
@@ -12,6 +12,7 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,21 +21,18 @@ public class MySqlShoppingCardDao extends  MySqlDaoBase implements ShoppingCartD
     private ProductDao productDao;
 
     @Autowired
-    public MySqlShoppingCardDao(DataSource dataSource) {
+    public MySqlShoppingCardDao(DataSource dataSource, ProductDao productDao) {
         super(dataSource);
+        this.productDao = productDao;
     }
 
     @Override
     public ShoppingCart getByUserId(int userId) {
         ShoppingCart shoppingCart = new ShoppingCart();
         String sql = """
-                SELECT * FROM shopping_cart WHERE userId = ?""";
+                SELECT * FROM shopping_cart WHERE user_id = ?""";
 
         Map<Integer, ShoppingCartItem> items = new HashMap<>();
-
-        int counter = 1;
-
-
 
         try(Connection connection = getConnection();
             PreparedStatement statement = connection.prepareStatement(sql)){
@@ -43,13 +41,13 @@ public class MySqlShoppingCardDao extends  MySqlDaoBase implements ShoppingCartD
 
             try(ResultSet resultSet = statement.executeQuery()) {
                 while(resultSet.next()){
+                    int productId = resultSet.getInt(2);
                     ShoppingCartItem item = new ShoppingCartItem();
-                    Product product = productDao.getById(resultSet.getInt(2));
+                    Product product = productDao.getById(productId);
                     int quantity = resultSet.getInt(3);
                     item.setProduct(product);
                     item.setQuantity(quantity);
-                    items.put(counter, item);
-                    counter++;
+                    items.put(productId, item);
                 }
             }
 
@@ -58,5 +56,55 @@ public class MySqlShoppingCardDao extends  MySqlDaoBase implements ShoppingCartD
         }
 
         return shoppingCart;
+    }
+
+    public void addItemToCart(int id, ShoppingCartItem item){
+        String sql = """
+                INSERT INTO shopping_cart (user_id,product_id,quantity) VALUES (?,?,?)""";
+
+        try(Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, id);
+            statement.setInt(2, item.getProductId());
+            statement.setInt(3, item.getQuantity());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void increaseQuantityByOne(int userId, int productId){
+        String sql = """
+                UPDATE shopping_cart SET quantity = quantity + 1 WHERE user_id = ? AND product_id = ?""";
+
+        try(Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, userId);
+            statement.setInt(2, productId);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void clearCart(int userId){
+        String sql= """
+                DELETE FROM shopping_cart where user_id = ?""";
+        try(Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)){
+
+            statement.setInt(1, userId);
+
+            statement.executeUpdate();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
